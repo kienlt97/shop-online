103bcf9ea775fd103eb7806fe8319315ab05967e
Luu Trung Kien <kienlt75@ghtk.co>
2023-08-29 18:27:11 +0700

    finish video 7

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Card } from 'antd';
-import { StyleNameProduct } from './style';
+import { StyleNameProduct, WrapperCardStyle, WrapperDiscountText, WrapperImageStyle, WrapperPriceText, WrapperReporText } from './style';
+import { StarFilled } from '@ant-design/icons';
+import official from '../../assets/images/official.png'
 
 const CardComponent = () => {
   return (
-    <Card
+    <WrapperCardStyle
         hoverable
-        style={{ width: 240 }}
+        headStyle={{width:'200px', height:'200px'}}
+        style={{ width: 200 }}
         bodyStyle={{padding:'10px'}}
         cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
     >
-    <StyleNameProduct>Inphone</StyleNameProduct>
-    </Card>
+        <WrapperImageStyle src={official} alt="official"/>
+        <StyleNameProduct>Inphone</StyleNameProduct>
+        <WrapperReporText>
+            <span style={{marginRight:'4px'}}>
+              <span>4.5</span>
+              <StarFilled style={{fontSize:'12px',color:'yellow'}}/>
+            </span>
+            <span>| Da ban 1000+</span>
+        </WrapperReporText>
+        <WrapperPriceText>
+            19.000.000 
+            <WrapperDiscountText>-5%</WrapperDiscountText> 
+        </WrapperPriceText>
+    </WrapperCardStyle>
   )
 }
 
--- src/components/CardComponent/style.js
+++ src/components/CardComponent/style.js
@@ -1,8 +1,49 @@
+import { Card } from "antd";
 import { styled } from "styled-components";
+
+export const WrapperCardStyle = styled(Card)`
+    width: 200px;
+    & img {
+        height: 200px;
+        width: 200px;
+    }
+    position: relative;
+`
 
 export const StyleNameProduct = styled.div`
     font-weight: 400;
     font-size: 12px;
     line-height: 16px;
     color: rgb(56, 56, 61);
+`
+
+export const WrapperReporText = styled.div`
+    font-size: 11px;
+    color: rgb(128, 128, 137);
+    display: flex;
+    align-items: center;
+    margin: 6px 0 4px;
+`
+
+export const WrapperPriceText = styled.div`
+    font-size: 16px;
+    font-weight: 500;
+    color: rgb(255, 66, 78);
+    margin: 4px 0;
+`
+
+export const WrapperDiscountText = styled.span`
+    font-size: 12px;
+    font-weight: 500;
+    color: rgb(255, 66, 78);
+`
+
+
+export const WrapperImageStyle = styled.img`
+    top: -1px;
+    left: -1px;
+    border-top-left-radius: 3px;
+    postition: absolute;
+    width: 68px !important;
+    height: 14px !important;
 `
